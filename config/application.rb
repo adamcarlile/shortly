2ecfb12844d6b38efb784daff353e36881cd048c
Adam Carlile <hello@adamcarlile.com>
2019-07-23 20:19:15 +0100

    Extract methods from the short class, and rely on the storage driver for storage implementation

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,11 @@ module Shortly
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.shortly = ActiveSupport::OrderedOptions.new.tap do |shortly|
+      shortly.store_adaptor = :threadsafe_memory_store
+    end 
   end
 end
+
+Shortly::Config = Rails.application.config.shortly
